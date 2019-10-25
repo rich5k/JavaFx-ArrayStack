75dfbb0a610c051bc54db80036b13d42a2a2cc73
Richard Anatsui <43499620+rich5k@users.noreply.github.com>
2019-10-25 14:45:39 +0100

    Add files via upload
    In-class projects

---
+++ Person.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author richa
+ */
+public class Person {
+     public static enum Gender{MALE, FEMALE};
+    private String name;
+    private Gender gender;
+    private int age;
+    
+    //default constructor that sets the name instance variable to "No name yet" and the age variable to zero
+    public Person(){
+        this.name= "No name yet";
+        this.age = 0;
+        this.gender= Gender.FEMALE;
+    }
+    
+    public Person(String name, Gender gender, int age){
+        this.name= name;
+        this.age= age;
+        this.gender=gender;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+     public int getAge(){
+        return this.age;
+    }
+    
+      public Gender getGender(){
+        return this.gender;
+    }
+      
+      public void setName(String n){
+          this.name=n;
+      }
+      
+      public void setName(String first, String last){
+          name=first+" "+ last;
+      }
+      
+      public void incrementAge(){
+          this.age++;
+      }
+}
---
+++ Vehicle.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author richa
+ */
+public class Vehicle {
+    private String l_plate;
+    private String make;
+    private String model;
+    private int manu_year;
+    private int doors;
+    private int wheels;
+    private Person driver;
+    private Person[] passengers;
+    private int currentPass;
+    
+    public Vehicle(String l_plate, String make, String model, int manu_year, int doors, )
+    
+}
